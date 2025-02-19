128cff2de613d5afc69f9fc68efc02656b551dcd
Joan Saura <116755457+JSaura04@users.noreply.github.com>
2025-02-19 19:14:08 +0100

    19/2

--- files/forms/reviews/review_control_panel.php
+++ files/forms/reviews/review_control_panel.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 $root = $_SERVER['DOCUMENT_ROOT'];
 include($root . '/student071/dwes/files/common-files/db_connection.php');
 include($root . '/student071/dwes/files/functions/displayStars.php');
-include($root . '/student071/dwes/files/common-files/header.php'); 
+include($root . '/student071/dwes/files/common-files/header.php');
 ?>
 
 <div id="review-control-panel">
     <h1>Review Control Panel</h1>
-    
+
     <?php
     $query = "
         SELECT r.review_id, r.customer_review, r.customer_score, r.inserted_on,r.review_title, u.user_online 
@@ -17,13 +17,13 @@ include($root . '/student071/dwes/files/common-files/header.php');
         ORDER BY r.inserted_on DESC
     ";
     $result = mysqli_query($conn, $query);
-    
+
     if ($result && mysqli_num_rows($result) > 0) {
-        while($review = mysqli_fetch_assoc($result)) {
-            ?>
+        while ($review = mysqli_fetch_assoc($result)) {
+    ?>
             <div class="review-card">
-            <h3><?php echo htmlspecialchars($review['user_online']); ?> - <?php echo displayStars($review['customer_score']); ?></h3>
-            <h5><?php echo htmlspecialchars($review['review_title']); ?></h5>
+                <h3><?php echo htmlspecialchars($review['user_online']); ?> - <?php echo displayStars($review['customer_score']); ?></h3>
+                <h5><?php echo htmlspecialchars($review['review_title']); ?></h5>
                 <p><?php echo htmlspecialchars($review['customer_review']); ?></p>
                 <small>Posted on: <?php echo htmlspecialchars($review['inserted_on']); ?></small>
                 <div class="review-actions">
@@ -31,7 +31,7 @@ include($root . '/student071/dwes/files/common-files/header.php');
                     <a href="/student071/dwes/files/querys/reviews/update_review_status.php?review_id=<?php echo $review['review_id']; ?>&action=reject" class="reject-button">Reject</a>
                 </div>
             </div>
-            <?php
+    <?php
         }
     } else {
         echo "<p>No pending reviews found.</p>";
@@ -39,6 +39,6 @@ include($root . '/student071/dwes/files/common-files/header.php');
     ?>
 </div>
 
-<?php 
-include($root . '/student071/dwes/files/common-files/footer.php'); 
+<?php
+include($root . '/student071/dwes/files/common-files/footer.php');
 ?>
